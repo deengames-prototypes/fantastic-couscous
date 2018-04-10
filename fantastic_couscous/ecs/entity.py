220ba037f2c0015fd7c082713c9b81249ef70ed9
nightblade9 <nightbladecodes@gmail.com>
2018-04-10 11:54:20 -0400

    Added `get_all` and `destroy`, with tests. Added `destroy_all` for fixture cleanup.

--- fantastic_couscous/ecs/entity.py
+++ fantastic_couscous/ecs/entity.py
@@ -1,6 +1,12 @@
 class Entity:
+
+    # Useful for Entity.get('tag'). But because of this, consumers of our ECS
+    # need to call `.destroy` when they're done with entities.
+    _all_entities = []
+
     def __init__(self):
         self.components = {}
+        Entity._all_entities.append(self)
     
     def set(self, component):
         key = type(component)
@@ -8,3 +14,21 @@ class Entity:
     
     def get(self, clazz):
         return self.components[clazz]
+
+    def destroy(self):
+        Entity._all_entities.remove(self)
+
+    """
+    Returns all entities of a specific class type (including subclasses).
+    eg. if you create a Wall entity class, Entity.get_all(Wall) returns that
+    instance, while Entity.get_all(Entity) returns every entity created
+    (including Wall, which subclasses Entity directly or indirectly).
+    """
+    @staticmethod
+    def get_all(clazz):
+        return [e for e in Entity._all_entities if isinstance(e, clazz)]
+    
+    # Used to clean up test fixtures quickly
+    @staticmethod
+    def destroy_all():
+        Entity._all_entities = []
--- tests/fantastic_couscous/ecs/test_entity.py
+++ tests/fantastic_couscous/ecs/test_entity.py
@@ -1,8 +1,12 @@
 import pytest
+import random
 
 from fantastic_couscous.ecs.entity import Entity
 
 class TestEntity:
+    def setup(self):
+        Entity.destroy_all()
+
     def test_getter_gets_set_values(self):
         e = Entity()
         expected = IntComponent(13)
@@ -11,7 +15,48 @@ class TestEntity:
         actual = e.get(IntComponent)
         assert actual == expected
 
+    def test_entity_getall_gets_all_entities_of_that_type(self):
+        w1 = Wall()
+        w2 = Wall()
+        t = Tree()
+
+        all_walls = Entity.get_all(Wall)
+        all_trees = Entity.get_all(Tree)
+
+        assert len(all_walls) == 2
+        assert all_walls[0] == w1
+        assert all_walls[1] == w2
+        
+        assert len(all_trees) == 1
+        assert all_trees[0] == t
+    
+    def test_entity_getall_gets_all_subclasses_of_that_type(self):
+        w = Wall()
+        m = MetalWall()
+        t = Tree()
+
+        all_walls = Entity.get_all(Wall)
+        assert len(all_walls) == 2
+        assert all_walls[0] == w
+        assert all_walls[1] == m
+
+    def test_entity_destroy_removes_entity_from_getall(self):
+        t = Tree()
+        assert Entity.get_all(Tree)[0] == t
+        t.destroy()
+
+        actual = Entity.get_all(Tree)
+        assert len(actual) == 0
 
 class IntComponent:
     def __init__(self, value):
         self.value = value
+
+class Wall(Entity):
+    pass
+
+class MetalWall(Wall):
+    pass
+
+class Tree(Entity):
+    pass
