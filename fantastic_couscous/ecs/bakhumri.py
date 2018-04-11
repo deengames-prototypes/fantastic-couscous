4863cbfa8366e52976765aa89bf593efec4ca6f1
NegativeScript <ala2aldeen913@gmail.com>
2018-04-11 07:43:13 +0300

    Moved docstring to correct location

--- fantastic_couscous/ecs/bakhumri.py
+++ fantastic_couscous/ecs/bakhumri.py
@@ -3,16 +3,16 @@ class Bakhumri:
     # need to call `.destroy` when they're done with entities.
     _all_entities = []
 
-    """
-    Returns all entities of a specific class type (including subclasses).
-    eg. if you create a Wall entity class, Entity.get_all(Wall) returns that
-    instance, while Entity.get_all(Entity) returns every entity created
-    (including Wall, which subclasses Entity directly or indirectly).
-    
-    eg. Bakhumri(Wall), Bakhumri(Entity)
-    """
     @staticmethod
     def __call__(component_class):
+        """
+        Returns all entities of a specific class type (including subclasses).
+        eg. if you create a Wall entity class, Entity.get_all(Wall) returns that
+        instance, while Entity.get_all(Entity) returns every entity created
+        (including Wall, which subclasses Entity directly or indirectly).
+
+        eg. Bakhumri(Wall), Bakhumri(Entity)
+        """
         return [e for e in Bakhumri._all_entities if e.has(component_class)]
 
     # Used to clean up test fixtures quickly
