71cc2dcffc86ceda1e0b5f14770a277c0137cdbc
nightblade9 <nightbladecodes@gmail.com>
2018-04-11 22:31:20 -0400

    Draw player on-screen.

--- fantastic_couscous/ecs/components/display_component.py
+++ fantastic_couscous/ecs/components/display_component.py
@@ -1,38 +1,6 @@
 class DisplayComponent:
-    def __init__(self, _character, colour, x, y):
-        self.character = _character
+    def __init__(self, character, colour, x, y):
+        self.character = character
         self.colour = colour
         self.x = x
         self.y = y
-
-    # @property
-    # def character(self):
-    #     return self._character
-    
-    # @colour.setter
-    # def character(self, value):
-    #     self._character = value
-    
-    # @property
-    # def colour(self):
-    #     return self._colour
-    
-    # @colour.setter
-    # def colour(self, value):
-    #     self._colour = value
-
-    # @property
-    # def x(self):
-    #     return self._x
-    
-    # @colour.setter
-    # def x(self, value):
-    #     self._x = value
-    
-    # @property
-    # def y(self):
-    #     return self._y
-    
-    # @colour.setter
-    # def y(self, value):
-    #     self._y = value
--- prototype/main.py
+++ prototype/main.py
@@ -1,17 +1,40 @@
+import os
+import sys
+# import ../*
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from fantastic_couscous.ecs.entity import Entity
+from fantastic_couscous.ecs.components.display_component import DisplayComponent
+
 import tdl
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__(DisplayComponent('@', 0xFFFFFF, 40, 25))
 
 class Main:
     def __init__(self):
-        pass
+        self.player = Player()
+        self.root_console = tdl.init(80, 50)
+        self.game_over = False
 
     def core_game_loop(self):
-        root_console = tdl.init(80, 25)
+        self.draw()
 
-        while not tdl.event.is_window_closed():
+        while not tdl.event.is_window_closed() and not self.game_over:
             user_input = tdl.event.key_wait()
             key_pressed = user_input.keychar
-            if key_pressed == "ESCAPE" or key_pressed == 'q':
-                break # game over
+            self.process_input(key_pressed)
+            # time passes
+            self.draw()
+    
+    def process_input(self, key_pressed):
+        if key_pressed == "ESCAPE" or key_pressed == 'q':
+            self.game_over = True
+
+    def draw(self):
+        dc = self.player.get(DisplayComponent)
+        self.root_console.draw_char(dc.x, dc.y, dc.character, dc.colour)
             
 
 if __name__ == "__main__":
